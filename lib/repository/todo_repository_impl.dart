d0b0e010545a533b12c4d8f63ff1bda6f2919c83
irmu98 <choiroom98@naver.com>
2025-04-02 15:32:18 +0900

    fix: toggleTodo 수정중

--- lib/repository/todo_repository_impl.dart
+++ lib/repository/todo_repository_impl.dart
@@ -8,48 +8,81 @@ class TodoRepositoryImpl implements TodoRepository {
   final TodoDataSource _dataSource;
 
   TodoRepositoryImpl({required TodoDataSource dataSource})
-    : _dataSource = dataSource;
+      : _dataSource = dataSource;
 
   @override
   Future<void> addTodo(String title) async {
-    List<Todo> todoList = await getTodos();
-
-    final newTodo = Todo(
-      userId: 1,
-      id: todoList.last.id + 1,
-      title: title,
-      completed: false,
-      createdAt: DateTime.now(),
-    );
-
-    todoList.add(newTodo);
-    
-    await _dataSource.writeTodos(todoList.map((e) => e.toJson()).toList());
-
-  }
-
-  @override
-  Future<void> deleteTodo(int id) {
-    // TODO: implement deleteTodo
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<List<Todo>> getTodos() async {
-    // TODO: implement getTodos]
     try {
-      final List<Map<String, dynamic>> todosJson =
-          await _dataSource.readTodos();
-      return todosJson.map((todo) => Todo.fromJson(todo)).toList();
-    } catch (e) {
-      throw Exception('');
+      List<Todo> todoList = await getTodos();
+
+      final newTodo = Todo(
+        userId: 1,
+        id: todoList.last.id + 1,
+        title: title,
+        completed: false,
+        createdAt: DateTime.now(),
+      );
+
+      todoList.add(newTodo);
+
+      await _dataSource.writeTodos(
+          todoList
+              .map((e) => e.toJson())
+              .toList());
+    } catch(e) {
+      throw Exception(e);
     }
   }
 
   @override
-  Future<void> toggleTodo(int id) {
-    // TODO: implement toggleTodo
-    throw UnimplementedError();
+  Future<void> deleteTodo(int id) async {
+    try {
+      List<Todo> todoList = await getTodos();
+
+      await _dataSource.writeTodos(
+          todoList
+              .where((e) => e.id != id)
+              .map((e) => e.toJson())
+              .toList());
+    } catch(e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  Future<List<Todo>> getTodos() async {
+    try {
+      final List<Map<String, dynamic>> todosJson =
+      await _dataSource.readTodos();
+      return todosJson.map((e) => Todo.fromJson(e)).toList();
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  Future<void> toggleTodo(int id) async {
+    try {
+      List<Todo> todoList = await getTodos();
+
+      List<Todo> sampleList = todoList.where((e) => e.id != id).toList();
+
+      sampleList.add(
+          todoList.where((e) => e.id == id).toList()
+      );
+
+      await _dataSource.writeTodos(
+          todoList
+              .map((e) {
+            if (e.id == id) {
+              e.completed = !e.completed;
+            }
+            return e.toJson();
+          })
+              .toList());
+    } catch(e) {
+      throw Exception(e);
+    }
   }
 
   @override
